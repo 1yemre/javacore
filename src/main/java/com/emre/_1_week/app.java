28641b18e70212a603741240c1010064e2c66dc6
Emre Enes Yenen <emreenesyenenn@gmail.com>
2025-02-08 18:45:09 +0300

    Java Basic

---
+++ src/main/java/com/emre/_1_week/app.java
@@ -0,0 +1,53 @@
+package com.emre._1_week;
+
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+          /*
+           1 Dereceden Bir bilinmeyenli denklem çözümü;
+             kullanıcıdan ax+b=0 denklemindeki a ve b değerini alıp x'i bulan program.
+         */
+               /*
+                    Scanner scanner=new Scanner(System.in);
+                    System.out.println("Lutfen sayi giriniz :  ");
+
+
+                    double aValue,bValue,result;
+
+                    System.out.println("lutfen a degerini giriniz : ");
+                    aValue=scanner.nextDouble();
+
+                    System.out.println("lutfen b degerini giriniz : ");
+                    bValue=scanner.nextDouble();
+
+                    result=(-bValue/aValue);
+                    System.out.println("a degeri:"+aValue+ " b degeri : "+bValue+ " sonuc : "+result);
+
+                    scanner.close();
+
+                */
+
+
+
+        /*
+           santigirat(celsius) Fahrenheit dönüştürücü ;
+           soru:
+                kullanıcıdan santigrat (celsius) cinsinden sıcaklık alıp  Fahrenheit'e çeviren program.
+                F=(c*9/5)+32
+        */
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
