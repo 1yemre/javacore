8a383067527d110ca057541d0837408aed7cbb95
Emre Enes Yenen <emreenesyenenn@gmail.com>
2025-02-08 15:06:59 +0300

    Java Basic

--- src/main/java/com/emre/_1_part_javatype/_01_JavaTypes.java
+++ src/main/java/com/emre/_1_part_javatype/_01_JavaTypes.java
@@ -1,4 +1,13 @@
 package com.emre._1_part_javatype;
 
 public class _01_JavaTypes {
+    // psvm => tab
+    public static void main(String[] args) {
+        //sout => tab
+        System.out.println("Emre");
+
+    }  // end psvm
 }
+
+
+
---
+++ src/main/java/com/emre/_1_part_javatype/_04_JavaWrapperTypes.java
@@ -0,0 +1,13 @@
+package com.emre._1_part_javatype;
+
+public class _04_JavaWrapperTypes {
+    public static void main(String[] args) {
+        // PRIMITIVE TYPE
+        // Veri değişken isimlerini yazarken;
+
+        // WRAPPER TYPE
+        // Primitive Type - Wrapper Typen Arasındaki farklar ?
+        // Stack
+    }
+
+}
