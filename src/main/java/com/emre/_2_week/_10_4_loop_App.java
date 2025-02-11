78d3fbb47c6216f5d7f2c768e0973f829ed84a4b
Emre Enes Yenen <emreenesyenenn@gmail.com>
2025-02-11 20:30:43 +0300

    Loop

---
+++ src/main/java/com/emre/_2_week/_10_4_loop_App.java
@@ -0,0 +1,42 @@
+package com.emre._2_week;
+
+import java.util.Scanner;
+
+public class _10_4_loop_App {
+    public static void main(String[] args) {
+  /*
+       // tek çift sayi ornegi
+        Scanner klavye =new Scanner(System.in);
+        System.out.println("Lutfen Pozitif Bir sayi giriniz: ");
+        int number = klavye.nextInt();
+
+        // validation
+        //1) negatif sayi olursa
+        //2) kullanici haarf girerse
+        //3) sıfır
+
+                if (number<0){
+                     System.out.println("Negatif bir sayi girdiniz !");
+                     //number=Math.abs(number);
+                     number=number+(-1);
+                 }
+                 else{
+                     if(number %2==0){
+                         System.out.println(number+ " sayi cift sayidir.");
+                     }else{
+                         System.out.println(number+" sayi Tek sayidir.");
+                     }
+                 }
+
+*/
+
+
+
+
+
+
+
+
+
+    }
+}
